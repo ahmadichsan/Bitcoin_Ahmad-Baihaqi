a5454fcb4ad66b956691e782558455c3f58b744b
Ichsan <ahmdichsanb@gmail.com>
2018-06-07 23:48:37 +0700

    First upload
    ancang soal 3

---
+++ Soal_3/soal3/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+// import axios from 'axios';
+
+class App extends Component 
+{
+  // componentDidMount()
+  // {
+  //   axios.get('Link').then((ambilData) => {
+  //     console.log(ambilData);
+  //   })
+  // };
+
+  render() 
+  {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
